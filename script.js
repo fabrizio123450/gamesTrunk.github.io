710c51df186de8c8fcbf6b687467db5172636397
unknown <fabrizio123450@gmail.com>
2023-12-15 20:47:43 -0300

    agregado link de juego 3

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 // Obtenemos el botón por su identificador
+let botonJuego1 = document.getElementById("game1");
 let botonJuego2 = document.getElementById("game2");
 let botonJuego3 = document.getElementById("game3");
 
-
+/**
+ * repo juego 1
+ * https://github.com/fabrizio123450/avion.github.io
+ */
 // Agregamos un evento de clic al botón
-botonJuego2.addEventListener("click", function() {
+botonJuego1.addEventListener("click", function() {
     // Redirige a otra_pagina.html
+    window.location.href = "https://fabrizio123450.github.io/avion.github.io/";
+});
+botonJuego2.addEventListener("click", function() {
     window.location.href = "mi juego de memoria/index.html";
 });
 botonJuego3.addEventListener("click", function() {
-    // Redirige a otra_pagina.html
     window.location.href = "wordle/index.html";
 });
